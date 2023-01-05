84f12bf444cf93149746dcc834cad60b082a0b39
rachosuar <rachosuar@gmail.com>
2023-01-05 11:07:42 -0300

    self-destruct function added

--- Contract.sol
+++ Contract.sol
@@ -4,21 +4,33 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SmartWallet {
-    ERC20 public token;
+
+ /// @notice Create smart wallet in order to the contract/wallet receives a metadata transaction from the user (gasless) and can transform that data into the execution of a function that has token, amount and recipient as parameters
+contract SmartWallet is Ownable {
+    ERC20 public _token;
+
+    bool whitelist;
 
     event transfer (address owner, address to, address token, uint256 value);
     event transferBatch (address owner, address [] to, address [] token, uint256 [] value);
 
- /// @notice Create smart wallet in order to the contract/wallet receives a metadata transaction from the user (gasless) and can transform that data into the execution of a function that has token, amount and recipient as parameters
+   modifier whenNotPaused () {
+        require(true,"contract is paused");
+   }
+
+   modifier IsWhitelisted(address){
+        require (whitelist(address),"Address not whitlisted");
+   }
+
+ /// @notice allows transfer ERC20 tokens between smart wallets
  /// @dev Juan Cruz Suarez - Matias Zapata - Constanza Oriana
  /// @param address owner - Sender account 
  /// @param address to - Receiver account 
- /// @param address token - 
+ /// @param address token - Token to send contract address
  /// @param uint256 value - Amount of tokens to transfer
  /// @param uint8 v, bytes32 r, bytes32 s - Validates secp256k1 signature
-
 function transferToken(
     address owner,
     address to,
@@ -27,8 +39,7 @@ function transferToken(
     uint8 v,
     bytes32 r,
     bytes32 s
-) external whenNotPausedIsWhitelisted(owner)
-isWhiteListed(to) {
+) external whenNotPaused IsWhitelisted(owner) IsWhitelisted(to) {
     _transferToken(owner, to, token, value, v, r, s);
 }
 
@@ -44,17 +55,16 @@ function _transferToken (
     bytes memory data = abi.encode(
         PERMIT_TYPEHASH, 
         owner,
-        spender,
+        to,
         value,
         _permitNonces[owner]++)
 ;
 require(
     EIP712.recover(DOMAIN_SEPARATOR, v, r, s, data) == owner, "Invalid signature");
-    token.transfer(to, value);
+    _token=ERC20(token);
+    _token.transfer(to, value);
 
-   
     emit transfer (owner, to, token, value);
-
 }
  /// @notice To transfer a batch of transactions
  /// @dev Juan Cruz Suarez - Matias Zapata - Constanza Oriana
@@ -74,7 +84,7 @@ function transferBatch(
     bytes32 r,
     bytes32 s
 ) external whenNotPaused IsWhitelisted(owner)
-isWhiteListedBatch(to) {
+IsWhitelisted(to) {
     _transferToken(owner, to, token, value, v, r, s);
 }
 
@@ -105,11 +115,19 @@ require(
     EIP712.recover(DOMAIN_SEPARATOR, v, r, s, data) == owner, "Invalid signature");
 
     for (uint i=0;i<to.length;i++){
-    token[i].transfer(to[i],value[i]);
+    _token=ERC20(token[i]);
+    _token[i].transfer(to[i],value[i]);
     }
     
 
     emit transferBatch (owner, to, token, value);
 
 }
+
+/// @notice If account is not been used, destoy and recover ETH
+ /// @dev Juan Cruz Suarez - Matias Zapata - Constanza Oriana
+function destroyAndRecover () public onlyOwner view {
+selfdestruct(owner);
+}
+
 }
